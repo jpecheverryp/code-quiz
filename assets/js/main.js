c017a2e7a28370fe75964e610082cf6e5cfac684
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2021-01-10 15:27:35 -0500

    add first question as an object

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,18 @@
+// INITIALIZATION
 var startButton = document.getElementById('start-button');
 var startHeading = document.getElementById('start-heading');
 var startDescription = document.getElementById('start-description');
 
+// Questions of the quiz
+var firstQuestion = {
+    // The question is a string that will be displayed
+    question: 'Which one is NOT a data type of Javascript:',
+    // The answers is an array with different strings that can be selected
+    answers: ['Boolean', 'Alert', 'String', 'Number'],
+    // The correctAnswer is the index of the correct answer in the array
+    // With Index 0
+    correctAnswer: 1
+}
 
 function removeStartScreen() {
     startHeading.remove();
@@ -11,6 +22,7 @@ function removeStartScreen() {
 
 function startQuiz() {
     removeStartScreen();
+    console.log(firstQuestion);
 }
 
 startButton.addEventListener('click', startQuiz)
